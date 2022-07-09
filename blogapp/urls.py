9edd67acaa927327f056cfe0737fe4bbda79710c
Cyber Worm <72599968+bellurm@users.noreply.github.com>
2022-07-09 20:43:48 +0300

    Update urls.py

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('ale/da/admincw/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('blogsite.urls')),
     path('ckeditor', include('ckeditor_uploader.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
